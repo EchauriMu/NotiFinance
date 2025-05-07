2b9d177fc5b63afe9d824632f00b23846da74b4a
EchauriMu <edechaurimu@ittepic.edu.mx>
2025-05-06 18:15:53 -0700

    priduccion v2

--- src/api/v1/routes/prediction.routes.js
+++ src/api/v1/routes/prediction.routes.js
@@ -8,26 +8,26 @@ const router = express.Router();
 
 // --- Rutas que requieren ser Analista ---
 // Crear nueva predicción
-router.post('/create', authenticateToken, isAnalyst, predictionController.handleCreatePrediction);
+router.post('/create',  isAnalyst, predictionController.handleCreatePrediction);
 
 // Actualizar una predicción existente
-router.patch('/update/:id', authenticateToken, isAnalyst, predictionController.handleUpdatePrediction);
+router.patch('/update/:id', isAnalyst, predictionController.handleUpdatePrediction);
 // O usa PUT si reemplazas completamente el recurso:
 // router.put('/update/:id', authenticateToken, isAnalyst, predictionController.handleUpdatePrediction);
 
 // Eliminar una predicción
-router.delete('/delete/:id', authenticateToken, isAnalyst, predictionController.handleDeletePrediction);
+router.delete('/delete/:id', isAnalyst, predictionController.handleDeletePrediction);
 
 
 // --- Rutas que requieren solo estar Autenticado (Analista o Basic) ---
 // Obtener todas las predicciones (con posibles filtros en query params)
-router.get('/get/all', authenticateToken, predictionController.handleGetAllPredictions);
+router.get('/get/all', predictionController.handleGetAllPredictions);
 
 // Obtener predicciones por símbolo de criptomoneda
-router.get('/get/symbol/:symbol', authenticateToken, predictionController.handleGetPredictionsBySymbol);
+router.get('/get/symbol/:symbol',  predictionController.handleGetPredictionsBySymbol);
 
 // Obtener una predicción específica por ID
-router.get('/get/:id', authenticateToken, predictionController.handleGetPredictionById);
+router.get('/get/:id',  predictionController.handleGetPredictionById);
 
 
 export default router;
